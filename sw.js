299c55af0201f58b028bcd277a70a9d3e6c84921
pratikkgadekar <pgadekar86@gmail.com>
2026-09-06 21:11:46 +0530

    Upgrade service worker cache to version 40

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
-/* sw.js - Service Worker Cache v25 (Force Purge) */
-const CACHE_NAME = 'mdt-cache-v25';
+/* sw.js - Service Worker Cache v40 */
+const CACHE_NAME = 'mdt-cache-v40';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/tools-part-1.js',
+  './js/tools-part-2.js',
+  './js/tools.js',
+  './js/core.js',
+  './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => caches.delete(key)))
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
     )
   );
   self.clients.claim();
@@ -16,6 +33,6 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
