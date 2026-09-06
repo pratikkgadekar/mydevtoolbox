cf73dede4656fe6f56395d5361fe9ff05059aa46
pratikkgadekar <pgadekar86@gmail.com>
2026-09-06 21:18:26 +0530

    Upgrade service worker cache to version 51
    Updated cache version and simplified cache management logic.

--- sw.js
+++ sw.js
@@ -1,31 +1,14 @@
-/* sw.js - Service Worker Cache v40 */
-const CACHE_NAME = 'mdt-cache-v40';
-const ASSETS = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/tools-part-1.js',
-  './js/tools-part-2.js',
-  './js/tools.js',
-  './js/core.js',
-  './manifest.json'
-];
+/* sw.js - Service Worker Cache v51 */
+const CACHE_NAME = 'mdt-cache-v51';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+      Promise.all(keys.map((key) => caches.delete(key)))
     )
   );
   self.clients.claim();
@@ -33,6 +16,6 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
