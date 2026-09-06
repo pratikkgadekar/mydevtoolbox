288d91772071980672169978f9054f0c76f0e535
pratikkgadekar <pgadekar86@gmail.com>
2026-09-06 18:35:27 +0530

    Update service worker cache version to v10

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* sw.js - Service Worker Cache v9 */
-const CACHE_NAME = 'mdt-cache-v9';
+/* sw.js - Service Worker Cache v10 */
+const CACHE_NAME = 'mdt-cache-v10';
 const ASSETS = [
   './',
   './index.html',
